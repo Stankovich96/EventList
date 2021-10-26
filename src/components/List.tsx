209640b1cbc62ce8464a6ea81c5cacd34717e46b
Stankovich96 <okikestan@gmail.com>
2021-10-27 00:06:51 +0100

    Displayed the render List function

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -10,20 +10,22 @@ interface IProps {
 }
 
 const List: React.FC<IProps> = ({ people }) => {
-	const renderList = () => {
+	const renderList = (): JSX.Element[] => {
 		return people.map((person) => {
-			<li className='List'>
-				<div className='List-header'>
-					<img className='List-img' src={person.url} alt='' />
-					<h2>{person.name}</h2>
-				</div>
-				<p>{person.age} years old</p>
-				<p className='List-note'>{person.note}</p>
-			</li>;
+			return (
+				<li className='List'>
+					<div className='List-header'>
+						<img className='List-img' src={person.url} alt='' />
+						<h2>{person.name}</h2>
+					</div>
+					<p>{person.age} years old</p>
+					<p className='List-note'>{person.note}</p>
+				</li>
+			);
 		});
 	};
 
-	return <ul></ul>;
+	return <ul>{renderList()}</ul>;
 };
 
 export default List;
