73ebf7615c0db001040a02a71d967cc2ba18a0ad
Stankovich96 <okikestan@gmail.com>
2021-11-03 00:09:27 +0100

    created a state for the form elements

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+interface IState {
+	name: string;
+	url: string;
+	age: string;
+	note?: string;
+}
 const AddToList = () => {
+	const [input, setInput] = useState<IState>({
+		name: '',
+		url: '',
+		age: '',
+		note: '',
+	});
+
+	const handleChange = () => {};
+
 	return (
 		<div className='AddToList'>
-			<input type='text' placeholder='Name' className='AddToList-input' />
-			<input type='text' placeholder='Age' className='AddToList-input' />
+			<input
+				type='text'
+				placeholder='Name'
+				className='AddToList-input'
+				value={input.name}
+				onChange={handleChange}
+				name='name'
+			/>
+			<input
+				type='text'
+				placeholder='Age'
+				className='AddToList-input'
+				value={input.age}
+				onChange={handleChange}
+				name='age'
+			/>
 			<input
 				type='text'
 				placeholder='Image Url'
 				className='AddToList-input'
+				value={input.url}
+				onChange={handleChange}
+				name='url'
 			/>{' '}
-			<textarea placeholder='Notes' className='AddToList-input' />
+			<textarea
+				placeholder='Notes'
+				className='AddToList-input'
+				value={input.note}
+				onChange={handleChange}
+				name='note'
+			/>
 		</div>
 	);
 };
